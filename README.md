bf3a20c8cc1dacb1385a9681cbbd078f70af4084
YVON Morgan <morgan@elysaa.net>
2016-01-15 21:08:19 +0100

    Add compile.sh to compile openwrt + qt5 Update install.sh to compile after install Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # openwrt-box
 
+```sh
 git clone git://github.com/myvon/openwrt-box.git --recursive
+chmod +x install.sh
+./install.sh
+```
---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+if [ "$1" != "debug" ]; then 
+	makeArg="-j5"
+else
+	makeArg="-j1 V=s"
+fi
+
+cd openwrt
+make prereq
+make $makeArg
+
